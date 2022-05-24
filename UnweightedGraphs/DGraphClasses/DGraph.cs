eb32718402c8108ba6c806069f799ace9529f23c
Joshua Allison <63320691+joshua-allison@users.noreply.github.com>
2022-05-23 18:31:48 -0700

    Added Class and Stubs

---
+++ UnweightedGraphs/DGraphClasses/DGraph.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DGraphClasses
+{
+    public class DGraph
+    {
+        public void AddNode(char node) => throw new NotImplementedException();
+        public void AddEdge(char node1, char node2) => throw new NotImplementedException();
+        public string ListNodes() => throw new NotImplementedException();
+        public string DisplayEdges() => throw new NotImplementedException();
+        public string DisplayMatrix() => throw new NotImplementedException();
+        public string BreadthFirst(char node) => throw new NotImplementedException();
+        public string DepthFirst(char node) => throw new NotImplementedException();
+        public string ConnectTable() => throw new NotImplementedException();
+        public string MinTree(char node) => throw new NotImplementedException();
+        public string MaxTree(char node) => throw new NotImplementedException();
+    }
+}
